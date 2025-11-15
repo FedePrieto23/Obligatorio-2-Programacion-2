602cb43dfde8ea12a696ef67f3264f7fccd7595b
Federico Priieto <fedeprieto23@gmail.com>
2025-11-15 14:23:57 -0300

    .

---
+++ Obligatorio 2/Pages/PageCliente/Baja.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Obligatorio_2.Pages.PageCliente
+{
+    public class BajaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
